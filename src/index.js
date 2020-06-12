80df088b3027c61944b4983bd14f519e1fb44fac
mishinkinakatya <mishinkinakatya@gmail.com>
2020-06-12 13:01:15 +0500

    Добавила и настроила компонент SmallMovieCard. Добавила валидацию с помощью prop-types

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import App from "./components/app/app.jsx";
 const MovieDescription = {
   TITLE: `The Grand Budapest Hotel`,
   GENRE: `Drama`,
-  YEAR: `2014`
+  YEAR: 2014
 };
+
+const MOVIE_TITLES = [`Fantastic Beasts: The Crimes of Grindelwald`, `Bohemian Rhapsody`, `Macbeth`, `Aviator`, `We need to talk about Kevin`];
 
 ReactDOM.render(
     <App
       title={MovieDescription.TITLE}
       genre={MovieDescription.GENRE}
       year={MovieDescription.YEAR}
+      movieTitles={MOVIE_TITLES}
     />,
     document.querySelector(`#root`)
 );
