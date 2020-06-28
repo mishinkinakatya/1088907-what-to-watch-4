5dffa0a0e2e6721a4c39e5754defeaba403e4f05
mishinkinakatya <mishinkinakatya@gmail.com>
2020-06-28 23:48:42 +0500

    Добавила снепшот тесты на компонент MoviePage

---
+++ src/components/movie-page/movie-page.test.js
@@ -0,0 +1,38 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import MoviePage from "./movie-page.jsx";
+
+const movie = {
+  title: `Movie_title-1`,
+  genre: `Genre`,
+  year: 1111,
+  poster: {
+    image: `Movie_image-1`,
+    title: `Movie_title-1`,
+  },
+  bgPoster: {
+    image: `Movie_image-1`,
+    title: `Movie_title-1`,
+  },
+  rating: {
+    score: 1.1,
+    count: 111
+  },
+  description: `Description of Movie_title-1.`,
+  director: `Director of Movie_title-1.`,
+  starring: `Starring of Movie_title-1.`,
+};
+
+describe(`MoviePage`, () => {
+  it(`Render MoviePage`, () => {
+    const tree = renderer
+      .create(
+          <MoviePage
+            movie={movie}
+          />
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
